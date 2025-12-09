ec08e6469c6f6d42988b6aefd71896d1a65c3e95
Kirill Taletski <kirill@taletski.com>
2025-12-09 16:40:55 +0300

    fix: add .env search debug

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,56 @@ mod templates;
 use anyhow::Result;
 use dotenvy::dotenv;
 
+use std::io::Write;
+
+fn debug_env_search() {
+    let cwd = std::env::current_dir().expect("failed to get CWD");
+    println!("CWD: {:?}", cwd);
+
+    match std::fs::read_dir(&cwd) {
+        Ok(_) => println!("can read dir"),
+        Err(e) => {
+            println!("can't read dir: {e}");
+            return;
+        },
+    };
+
+    let env_path = cwd.join(".env");
+    match std::fs::read_to_string(env_path) {
+        Ok(env) => println!("Read .env successfuly: {env}"),
+        Err(err) => println!("Failed to read .env: {err}"),
+    };
+
+    for path in cwd.ancestors() {
+        println!("-------------");
+        println!("reading ancestor: {:?}", path);
+
+        match std::fs::read_dir(path) {
+            Ok(_) => println!("can read dir"),
+            Err(e) => {
+                println!("can't read dir: {e}");
+                continue;
+            },
+        };
+
+        let env_path = path.join(".env");
+        if !env_path.exists() {
+            println!("no .env here");
+            continue;
+        };
+
+        match std::fs::read_to_string(env_path) {
+            Ok(env) => println!("Read .env successfuly: {env}"),
+            Err(err) => println!("Failed to read .env: {err}"),
+        };
+    }
+    std::io::stdout().flush().expect("failed to flush stdout");
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    debug_env_search();
+
     dotenv().expect("Failed to init dotenv");
 
     let config = config::AppConfig::init();
